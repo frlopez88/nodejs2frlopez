573c9ab7689df09b88e9929c7655eea83b0cf0a5
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-07-26 20:59:03 -0600

    Ultimo Commit Session 2

--- ServerS2S2.js
+++ ServerS2S2.js
@@ -1,7 +1,7 @@
 var http = require('http');
 //var modulo = require('./moduloprueba');
 var manejoUrl = require('url');
-
+var archivos = require('fs');
 
 http.createServer( function(req, res){
 
@@ -12,6 +12,25 @@ http.createServer( function(req, res){
 
 	var parametros =  manejoUrl.parse(req.url, true).query;
 
-	res.end("Nombre :" + parametros.nombre + " Edad: " + parametros.edad + " Nacionalidad: "+ parametros.nacionalidad );
+	if (parametros.pagina = 'Hola'){
+
+		 archivos.readFile('pagina.html', function(err, data) {
+		    res.writeHead(200, {'Content-Type': 'text/html'});
+		    res.write(data);
+		    return res.end();
+		  });
+	}
+
+	if (parametros.pagina = 'mundo'){
+		
+		archivos.readFile('mundo.html', function(err, data) {
+		    res.writeHead(200, {'Content-Type': 'text/html'});
+		    res.write(data);
+		    return res.end();
+		  });
+
+	}
+
+	//res.end("Nombre :" + parametros.nombre + " Edad: " + parametros.edad + " Nacionalidad: "+ parametros.nacionalidad );
 
 }).listen(8081);
